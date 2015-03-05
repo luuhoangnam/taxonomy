3bfeb27917b6a1694e51f1c0be8437b26331443e
Luu Hoang Nam <hoangnam0705@icloud.com>
2015-03-05 09:54:34 +0700

    Add service provider

---
+++ src/TaxonomyServiceProvider.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Namest\Taxonomy;
+
+use Illuminate\Support\ServiceProvider;
+use Namest\Sluggable\SluggableServiceProvider;
+
+/**
+ * Class TaxonomyServiceProvider
+ *
+ * @author  Nam Hoang Luu <nam@mbearvn.com>
+ * @package Namest\Taxonomy
+ *
+ */
+class TaxonomyServiceProvider extends ServiceProvider
+{
+    /**
+     * Boot up resources
+     */
+    public function boot()
+    {
+        // Publish a config file
+//        $this->publishes([
+//            __DIR__ . '/../config/taxonomy.php' => config_path('taxonomy.php')
+//        ], 'config');
+
+        // Publish your migrations
+        $this->publishes([
+            __DIR__ . '/../database/migrations/' => base_path('/database/migrations')
+        ], 'migrations');
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->register(new SluggableServiceProvider($this->app));
+    }
+}
